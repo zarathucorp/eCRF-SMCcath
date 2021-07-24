1348988e356fbf63ca98488197ab8d4d7feb3aa6
hjko <ghj20345@gmail.com>
2021-07-24 22:28:27 +0900

    displays a plot of an arbitrary dataframe on the main dashboard

--- R/patientsNumber_plot_module.R
+++ R/patientsNumber_plot_module.R
@@ -8,36 +8,21 @@ patientsNumber_plot_module_ui <- function(id) {
   fluidRow(
     column(
       width = 6,
-      echarts4rOutput("patientsNumber")
+      echarts4rOutput(ns("patientsNumber"))
     )
   )
 }
 
 patientsNumber_plot_module <- function(input, output, session) {
-  patinetsNumber <- 0
-  
-  tryCatch(
-    {
-      patinetsNumber <- conn %>%
-                        tbl('pros') %>%
-                        collect() %>% count(pid)
-    },
-    error = function(err) {
-      msg <- "Database Connection Error"
-      # print `msg` so that we can find it in the logs
-      print(msg)
-      # print the actual error to log it
-      print(error)
-      # show error `msg` to user.  User can then tell us about error and we can
-      # quickly identify where it cam from based on the value in `msg`
-      showToast("error", msg)
-    }
-  )
+  hospital <- c("Samsung", "Asan")
+  patients <- c(20, 70)
+  patientsNumber <- data.frame(hospital, patients)
+  print(patientsNumber)
   
   output$patientsNumber <- renderEcharts4r({
-    patientsNumber |> 
-      e_charts(x) |>
-      e_bar(y, name ="Hospital 1") |>
+    patientsNumber %>%
+      e_charts(hospital) %>%
+      e_bar(patients) %>%
       e_title("Patients Number")
   })
 }
