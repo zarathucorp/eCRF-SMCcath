ea857e880830bb766022ba50edc9e699400e305b
hjko <ghj20345@gmail.com>
2021-07-24 20:09:53 +0900

    server part of the patients number plotting module

--- R/patientsNumber_plot_module.R
+++ R/patientsNumber_plot_module.R
@@ -4,7 +4,36 @@ patientsNumber_plot_module <- function(id) {
   fluidRow(
     column(
       width = 6,
-      echarts4rOutput("patientNumber")
+      echarts4rOutput("patientsNumber")
     )
   )
+}
+
+patientsNumber_plot_module <- function(input, output, session) {
+  patinetsNumber <- 0
+  
+  tryCatch(
+    {
+      patinetsNumber <- conn %>%
+                        tbl('pros') %>%
+                        collect() %>% count(pid)
+    },
+    error = function(err) {
+      msg <- "Database Connection Error"
+      # print `msg` so that we can find it in the logs
+      print(msg)
+      # print the actual error to log it
+      print(error)
+      # show error `msg` to user.  User can then tell us about error and we can
+      # quickly identify where it cam from based on the value in `msg`
+      showToast("error", msg)
+    }
+  )
+  
+  output$patientsNumber <- renderEcharts4r({
+    patientsNumber |> 
+      e_charts(x) |>
+      e_bar(y, name ="Hospital 1") |>
+      e_title("Patients Number")
+  })
 }
