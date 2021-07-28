19888cdfdfd8e68adf4ffdae2c7fa2b6ce6d03da
hjko <ghj20345@gmail.com>
2021-07-28 23:27:37 +0900

    module for date selecting

---
+++ R/date_select_module.R
@@ -0,0 +1,17 @@
+date_select_module_ui <- function(id) {
+  ns <- NS(id)
+  
+  fluidRow(
+    selectInput(ns("month"), "Month", c(1:12)),
+    selectInput(ns("year"), "Year", c(2020:2025))
+  )
+}
+
+date_select_module <- function(input, output, session) {
+  return {
+    list(
+      month = reactive({input$month}),
+      year = reactive({input$year})
+    )
+  }
+}
