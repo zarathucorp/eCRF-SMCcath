c368cfd8d43fd77239ed544268c33f65e04bc6d2
hjko <ghj20345@gmail.com>
2021-07-29 02:23:42 +0900

    patients by date plot module works with arbitrary data set
    ** data manipulation is still under progress

---
+++ R/patientsByDate_plot_module.R
@@ -0,0 +1,36 @@
+patientsByDate_plot_module_ui <- function(id) {
+  ns <- NS(id)
+  
+  fluidRow(
+    column(
+      width = 12,
+      echarts4rOutput(ns("PatientsNumber")) %>%
+        withSpinner()
+    )
+  )
+}
+
+patientsByDate_plot_module <- function(input, output, session, data) {
+  date <- callModule(date_select_module, "date_selector")
+  observeEvent(date$month(), {
+    observeEvent(date$year(), {
+      dateQuery <- paste0(date$month(), "/", date$year())
+      
+      data$created_at = format(data$created_at, "%m/%Y")
+      subset <- filter(data, data$created_at == dateQuery)
+      selectedPatients <- as.data.frame(table(subset$Center))
+      
+      patientsPerDate <- data.frame(hospital = unique(data$Center), patients = 0)
+      for(i in 1:nrow(selectedPatients)) {
+        patientsPerDate[i, "patients"] = 1
+      }
+
+      output$PatientsNumber <- renderEcharts4r({
+        patientsPerDate %>%
+          e_charts(hospital) %>%
+          e_bar(patients) %>%
+          e_title("Patients Number By Date")
+      })
+    })
+  })
+}
